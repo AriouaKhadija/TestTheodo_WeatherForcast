7247a1202b12970fb18c15f6f16c73bafebf4499
KHADIJA ARIOUA <karioua@pca.ma>
2025-03-28 17:06:17 +0000

    - Add AsyncStorage to save data and display it offline -change api endpoint (weather instead forcast)

--- src/store/weatherSlice.js
+++ src/store/weatherSlice.js
@@ -1,38 +1,53 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axiosConfig from "../config/axiosConfig";
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axiosConfig from '../config/axiosConfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Action pour récupérer les données météo
-export const fetchWeather = createAsyncThunk("weather/fetchWeather", async () => {
-  try {
-    const response = await axiosConfig.get("/forcast");
-    console.log("Réponse API:", response.data);
-    
-    return response.data;
-  } catch (error) {
-    console.error("Erreur lors de la récupération des données météo :", error);
-  }
-});
+export const fetchWeather = createAsyncThunk(
+  'weather/fetchWeather',
+  async () => {
+    try {
+      const response = await axiosConfig.get('/weather');
+      // Sauvegarder les données dans le cache
+      await AsyncStorage.setItem(
+        'weatherCache',
+        JSON.stringify({
+          data: response.data,
+        }),
+      );
 
-const initialState={ forecast: [], status: "idle", error: null };
+      return response.data;
+    } catch (error) {
+      console.error('Erreur lors de la récupération des données météo :', error);
 
-const WeatherSlice = createSlice({
-  name: "weather",
-  initialState: initialState,
+      //récupérer les données depuis le cache si disponible
+      const cachedData = await AsyncStorage.getItem('weatherCache');
+      if (cachedData) {
+        const parsedData = JSON.parse(cachedData);
+        return parsedData.data;
+      }
+      throw new Error('Données météo indisponibles et pas de cache valide.');
+    }
+  },
+);
+
+const weatherSlice = createSlice({
+  name: 'weather',
+  initialState: {forecast: [], status: 'idle', error: null},
   reducers: {},
-  extraReducers: (builder) => {
+  extraReducers: builder => {
     builder
-      .addCase(fetchWeather.pending, (state) => {
-        state.status = "loading";
+      .addCase(fetchWeather.pending, state => {
+        state.status = 'loading';
       })
       .addCase(fetchWeather.fulfilled, (state, action) => {
-        state.status = "succeeded";
+        state.status = 'succeeded';
         state.forecast = action.payload;
       })
       .addCase(fetchWeather.rejected, (state, action) => {
-        state.status = "failed";
+        state.status = 'failed';
         state.error = action.error.message;
       });
   },
 });
 
-export default WeatherSlice.reducer;
+export default weatherSlice.reducer;
